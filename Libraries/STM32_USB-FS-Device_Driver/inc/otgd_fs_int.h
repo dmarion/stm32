5efd66bcaa66df882fe0e0aa5700919511aa2076
Damjan Marion <damjan.marion@gmail.com>
2011-07-24 00:39:20 +0200

    STM32_USB-FS-Device_Driver v3.3.0

--- Libraries/STM32_USB-FS-Device_Driver/inc/otgd_fs_int.h
+++ Libraries/STM32_USB-FS-Device_Driver/inc/otgd_fs_int.h
@@ -1,10 +1,9 @@
 /******************** (C) COPYRIGHT 2011 STMicroelectronics ********************
-* File Name          : usb_int.h
+* File Name          : otgd_fs_int.h
 * Author             : MCD Application Team
 * Version            : V3.3.0
 * Date               : 21-March-2011
-* Description        : Endpoint CTR (Low and High) interrupt's service routines
-*                      prototypes
+* Description        : Endpoint interrupt's service routines prototypes.
 ********************************************************************************
 * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
 * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE TIME.
@@ -23,8 +22,28 @@
 /* Exported constants --------------------------------------------------------*/
 /* Exported macro ------------------------------------------------------------*/
 /* Exported functions ------------------------------------------------------- */
-void CTR_LP(void);
-void CTR_HP(void);
+
+#ifdef STM32F10X_CL
+
+/* Interrupt Handlers functions */
+uint32_t OTGD_FS_Handle_Sof_ISR(void);
+uint32_t OTGD_FS_Handle_RxStatusQueueLevel_ISR(void);
+uint32_t OTGD_FS_Handle_GInNakEff_ISR(void);
+uint32_t OTGD_FS_Handle_GOutNakEff_ISR(void);
+uint32_t OTGD_FS_Handle_EarlySuspend_ISR(void);
+uint32_t OTGD_FS_Handle_USBSuspend_ISR(void);
+uint32_t OTGD_FS_Handle_UsbReset_ISR(void);
+uint32_t OTGD_FS_Handle_EnumDone_ISR(void);
+uint32_t OTGD_FS_Handle_IsoOutDrop_ISR(void);
+uint32_t OTGD_FS_Handle_EOPF_ISR(void);
+uint32_t OTGD_FS_Handle_EPMismatch_ISR(void);
+uint32_t OTGD_FS_Handle_InEP_ISR(void);
+uint32_t OTGD_FS_Handle_OutEP_ISR(void);
+uint32_t OTGD_FS_Handle_IncomplIsoIn_ISR(void);
+uint32_t OTGD_FS_Handle_IncomplIsoOut_ISR(void);
+uint32_t OTGD_FS_Handle_Wakeup_ISR(void);
+
+#endif /* STM32F10X_CL */
 
 /* External variables --------------------------------------------------------*/
 
